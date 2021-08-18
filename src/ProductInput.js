110d160dd902ba6b828893501d08ae4d08b898af
sun <sunpeiting07@gmail.com>
2021-08-18 12:41:47 +0930

    Update ProductInput.js
    set up search-input and sort-input structure in productInput Component

--- src/ProductInput.js
+++ src/ProductInput.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react'
 export default class ProductInput extends Component {
 	render() {
 		return (
-			<div>
-				ProductInput
+			<div className='product-input'>
+				<div className='search-field'>
+					<span className='search-field-name'>Search: </span>
+					<div className='search-field-name-input'>
+						<input />
+						<button>Search</button>
+					</div>
+				</div>
+				<div className='sort-field'>
+					<span className='sort-field-name'>Sort By:</span>
+					<div className='sort-field-input'>
+						<select ></select>
+					</div>
+				</div>
 			</div>
 		)
 	}
