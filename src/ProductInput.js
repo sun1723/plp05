a1fc02b435e328e5d93dd358d1d339e39ae36b1e
sun <sunpeiting07@gmail.com>
2021-08-18 13:26:31 +0930

    Update ProductInput.js
    create states and set logic for handling state changes

--- src/ProductInput.js
+++ src/ProductInput.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react'
 
 export default class ProductInput extends Component {
+	constructor( ){
+		super()
+		this.state={
+			keyword:'',
+			sort:''
+		}
+	}
+
+	handleKeywordSearch(event) {
+		this.setState({
+			keyword:event.target.value,
+		})
+	}
+	handleSortingChange(event) {
+		this.setState({
+			sort:event.target.value,
+		})
+	}
 	render() {
 		return (
 			<div className='product-input'>
 				<div className='search-field'>
 					<span className='search-field-name'>Search: </span>
 					<div className='search-field-name-input'>
-						<input />
+						<input value={this.state.keyword} onChange={this.handleKeywordSearch.bind(this)} />
 						<button>Search</button>
 					</div>
 				</div>
 				<div className='sort-field'>
-					<span className='sort-field-name'>Sort By:</span>
+					<label className='sort-field-name'for='dropdown'>Sort By:</label>
 					<div className='sort-field-input'>
-						<select ></select>
+						<select id="dropdown" value={this.state.sort} onChange={this.handleSortingChange.bind(this)} >
+							<option value="1">1</option>
+							<option value="2">2</option>
+						</select>
 					</div>
 				</div>
 			</div>
